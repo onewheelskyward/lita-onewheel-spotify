5e51d38df2187cf4a4557cd8dbb0619bfefbd8c8
Andrew Kreps <andrew.kreps@gmail.com>
2014-12-28 23:41:57 -0800

    trying a new string concatenator technique.

--- lib/lita/handlers/spotify.rb
+++ lib/lita/handlers/spotify.rb
@@ -17,11 +17,11 @@ module Lita
       http.get '/spotify/authorize', :authorize
 
       def handle_auth(response)
-        uri = 'https://accounts.spotify.com/authorize/?'
-        + "client_id=#{config.client_id}"
-        + '&response_type=code'
-        + '&redirect_uri=http://54.69.102.36:8182/spotify/authorize'
-        + '&scope=' + %w(playlist-read-private playlist-modify-public playlist-modify-private user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-email).join('%20')
+        uri = 'https://accounts.spotify.com/authorize/?'\
+        "client_id=#{config.client_id}"\
+        '&response_type=code'\
+        '&redirect_uri=http://54.69.102.36:8182/spotify/authorize'\
+        '&scope=' + %w(playlist-read-private playlist-modify-public playlist-modify-private user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-email).join('%20')
         # + '&state='
 
         Lita.logger.debug "Sending request to #{uri}"
