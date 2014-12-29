ec9146eae42abcbcfda5fe8438190275a1bfca5f
Andrew Kreps <andrew.kreps@gmail.com>
2014-12-28 16:45:13 -0800

    A good start.

--- lib/lita/handlers/spotify.rb
+++ lib/lita/handlers/spotify.rb
@@ -1,7 +1,40 @@
+require 'rspotify'
+
 module Lita
   module Handlers
     class Spotify < Handler
+      route(/^!spotify search artist (.*)/, :handle_artist_search)
+      route(/^!spotify search track (.*)/, :handle_track_search)
+      route(/^!spotify search album (.*)/, :handle_album_search)
 
+      def handle_artist_search(response)
+        search_term = response.matches[0][0]
+        artists = RSpotify::Artist.search(search_term)
+        if artists.count > 0
+          artist = artists.first
+          response.reply "#{artist.name}, #{artist.popularity}%.  #{artist.external_urls['spotify']}"
+        end
+      end
+
+      def handle_track_search(response)
+        search_term = response.matches[0][0]
+        tracks = RSpotify::Track.search(search_term)
+        if tracks.count > 0
+          track = tracks.first
+          artist_name = (track.artists.count > 0) ? track.artists.first.name : ''
+          response.reply "#{artist_name} - #{track.name}.  #{track.external_urls['spotify']}"
+        end
+      end
+
+      def handle_album_search(response)
+        search_term = response.matches[0][0]
+        albums = RSpotify::Album.search(search_term)
+        if albums.count > 0
+          album = albums.first
+          artist_name = (album.artists.count > 0) ? album.artists.first.name : ''
+          response.reply "#{artist_name} - #{album.name}.  #{album.external_urls['spotify']}"
+        end
+      end
 
     end
 
--- spec/lita/handlers/spotify_spec.rb
+++ spec/lita/handlers/spotify_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Lita::Handlers::Spotify, lita_handler: true do
-  it 'searches spotify' do
+  it 'searches spotify artists' do
     send_message '!spotify search artist rush'
-    expect(replies.last).to eq('rush')
+    expect(replies.last).to include('https://open.spotify.com/artist/2Hkut4rAAyrQxRdof7FVJq')
+  end
+
+  it 'searches spotify tracks' do
+    send_message '!spotify search track tom sawyer'
+    expect(replies.last).to eq('Rush - Tom Sawyer.  https://open.spotify.com/track/1NSXPZZHSIfvEqCSxiDop7')
+  end
+
+  it 'searches spotify albums' do
+    send_message '!spotify search album permanent waves'
+    expect(replies.last).to eq('Rush - Permanent Waves.  https://open.spotify.com/album/2gHaOUnuXm0VheySMhvImb')
   end
 end
